00fcfcb48c34c5f816ee72af411102b0b4d33376
Sambo <beckinghamcooks@gmail.com>
2016-05-26 19:40:36 +0100

    Added bootstrap, jquery, completed initial page and redirects.

--- app.js
+++ app.js
@@ -18,10 +18,6 @@ var Distributor = mongoose.model('Distributor', {
     lastName: String
 });
 
-app.get('/', function(req, res) {
-    //Get "Enter dist number" page
-});
-
 //Get dist name for confirmation modal
 app.get("/dist/:accountNo", function(req, res) {
     Distributor.findOne({account: req.params.accountNo}, function(err, distributor) {
@@ -35,11 +31,11 @@ app.get("/dist/:accountNo", function(req, res) {
 
 
 app.get('/order/:accountNo', function(req, res) {
-    //Get "Scan your items page"
+    //TODO: Get "Scan your items page"
 });
 
 app.get('/item/:itemNo', function(req, res) {
-   //Get item when barcode scanned
+   //TODO: Get item when barcode scanned
 });
 
 //Submit order object
@@ -49,8 +45,7 @@ app.post("/order", function(req, res) {
         if (err) {
             res.status(404).send(err)
         }
-
-        //Probably best to redirect to success page which auto directs to home.
+        
         res.status(200).send('order submitted!')
     })
 
--- js/site.js
+++ js/site.js
@@ -1,3 +1,25 @@
 /**
  * Created by Sambo on 26/05/2016.
  */
+
+function getDist() {
+    var distNo = $('#distNo').val();
+
+    $.ajax({
+        type: 'GET',
+        url: "localhost:3000/dist/" + distNo,
+        cache: false,
+        success: function(json){
+            $('#modal-distNo').text(json.accountNo);
+            $('#modal-distName').text(json.firstName + json.lastName);
+            $('#confirmModal').modal('show');
+        }
+        //TODO: Handle error (Error modal, perhaps?)
+    });
+
+    return false;
+}
+
+$('#modal-confirm').on('click', function(){
+    window.location.href = 'order.html?distNo=' + $('distNo').val();
+});
